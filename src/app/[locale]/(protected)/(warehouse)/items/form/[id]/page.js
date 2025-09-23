ea2369044845e1817f72de186b7db0011e30d91d
Moaz Ahmad <moaz1126@MacBook-Air.local>
2025-09-23 19:47:51 +0300

    Refactor: Use NotFound component for error handling in forms
    - Implemented  component in , , and \n- Provides consistent UI for missing or invalid data cases\n- Improves user experience by displaying a clear error state instead of blank or broken forms

--- src/app/[locale]/(protected)/(warehouse)/items/form/[id]/page.js
+++ src/app/[locale]/(protected)/(warehouse)/items/form/[id]/page.js
@@ -1,17 +1,24 @@
 import { getItem } from "../../actions"
-import ErrorLoading from "@/components/ErrorLoading"
+// import ErrorLoading from "@/components/ErrorLoading"
 import ItemsForm from "../page"
+import NotFound from "@/components/NotFound"
+import { getTranslations } from "next-intl/server"
 
 async function page({ params }) {
   const { id } = await params
   const res = await getItem(id)
+  const t = await getTranslations("")
 
   return (
       <div>
           {res.data?.id ? (
               <ItemsForm obj={res.data} />
           ) : (
-              <ErrorLoading name={"warehouse.items.form"} />
+              <NotFound 
+                name={t("warehouse.items.form.error")} 
+                // customButton={{ href: "/items/list", label: "Home", icon: <HomeIcon className="w-5 h-5" /> }} 
+              />
+            //   <ErrorLoading name={"warehouse.items.form"} />
           ) }
       </div>
   )
--- src/app/[locale]/(protected)/customer-supplier/form/[id]/page.js
+++ src/app/[locale]/(protected)/customer-supplier/form/[id]/page.js
@@ -1,17 +1,24 @@
+import { getTranslations } from "next-intl/server"
 import { getCS } from "../../actions"
 import CSForm from "../page"
-import ErrorLoading from "@/components/ErrorLoading"
+import NotFound from "@/components/NotFound"
+// import ErrorLoading from "@/components/ErrorLoading"
 
 async function page({ params }) {
     const id = (await params).id
     const CS = await (await getCS(id)).data
+    const t = await getTranslations("")
 
     return (
         <div>
             {CS?.id ? (
                 <CSForm obj={CS} />
             ) : (
-              <ErrorLoading name={"global.errors"} err="pageNotFound" />
+            <NotFound 
+                name={t("customer-supplier.form.error")} 
+                // customButton={{ href: "/customer-supplier/list", label: "Home", icon: <HomeIcon className="w-5 h-5" /> }} 
+            />
+            //   <ErrorLoading name={"global.errors"} err="pageNotFound" />
             ) }
         </div>
     )
