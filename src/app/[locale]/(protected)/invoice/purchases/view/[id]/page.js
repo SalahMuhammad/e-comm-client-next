ea4fbd7e53484276f50f6e281527cc94dbc43c95
SalahMuhammad <salahnazeer93@gmail.com>
2025-09-25 18:26:52 +0300

    fix invalid api url

--- src/app/[locale]/(protected)/invoice/purchases/view/[id]/page.js
+++ src/app/[locale]/(protected)/invoice/purchases/view/[id]/page.js
@@ -3,7 +3,7 @@ import { getInv } from "../../../common/actions";
 
 export async function generateMetadata({ params }) {
     const id = (await params).id
-    const invoice = (await getInv('sales', id)).data
+    const invoice = (await getInv('purchases', id)).data
 
     return {
         title: invoice?.id ? `${invoice.owner_name} - ${invoice.issue_date} - order No#${invoice.hashed_id}` : 'Invoice Not Found',
