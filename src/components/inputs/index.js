f6e264b66067c4766df57ca2fd4a70f153117ce8
SalahMuhammad <salahnazeer93@gmail.com>
2026-01-24 11:45:58 +0200

    refactor

--- src/components/inputs/index.js
+++ src/components/inputs/index.js
@@ -2,6 +2,6 @@ export { default as TextInput } from './TextInput';
 export { default as NumberInput } from './NumberInput';
 export { default as FileInput } from './FileInput';
 export { default as TextAreaInput } from './TextArea';
-export { default as DateInput } from './DateInput';
+export { default as DateInput } from './date/DateInput';
 export { default as QuantityInput } from './QuantityInput';
 export { default as RadioSwitch } from './RadioSwitch';
