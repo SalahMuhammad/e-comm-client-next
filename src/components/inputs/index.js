fa4d767b641ee651573fdf4f246a90b3eb01734b
SalahMuhammad <salahnazeer93@gmail.com>
2026-01-24 11:47:54 +0200

    refactor number inputs

--- src/components/inputs/index.js
+++ src/components/inputs/index.js
@@ -1,7 +1,7 @@
 export { default as TextInput } from './TextInput';
-export { default as NumberInput } from './NumberInput';
+export { default as NumberInput } from './number/NumberInput';
 export { default as FileInput } from './FileInput';
 export { default as TextAreaInput } from './TextArea';
 export { default as DateInput } from './date/DateInput';
-export { default as QuantityInput } from './QuantityInput';
+export { default as QuantityInput } from './number/QuantityInput';
 export { default as RadioSwitch } from './RadioSwitch';
