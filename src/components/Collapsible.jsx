ffc7c5e964c4ccf301649a5f302ad1082837da72
SalahMuhammad <salahnazeer93@gmail.com>
2026-07-21 13:06:50 +0300

    (update)

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import * as Collapsible from "@radix-ui/react-collapsible";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
-function MyCollapsible({ children }) {
+function MyCollapsible({ children, title }) {
     const [open, setOpen] = useState(false);
     const t = useTranslations("warehouse.items.form.collapsible");
 
@@ -17,7 +17,7 @@ function MyCollapsible({ children }) {
                     className="flex items-center justify-between w-full p-2 rounded-lg cursor-pointer text-left focus:outline-none hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors group"
                     aria-label="Toggle Price Inputs"
                 >
-                    <span className="text-lg font-semibold text-gray-800 dark:text-white">{t("prices")}</span>
+                    <span className="text-lg font-semibold text-gray-800 dark:text-white">{title}</span>
                     <div className="flex items-center gap-1 text-blue-600 dark:text-blue-400 group-hover:underline">
                         {open ? t("hide") : t("show")} {t("inputs")}
                         <ChevronDownIcon
